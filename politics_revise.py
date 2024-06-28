7331e52e1c439dcf7e4363256365ed7bde0f42df
dayeonkimm <ekdyd516@gmail.com>
2024-06-28 23:30:46 +0900

    scraped_urls 수정완료

--- politics_revise.py
+++ politics_revise.py
@@ -12,9 +12,15 @@ async def main():
         current_time = datetime.now().strftime("%Y-%m-%d_%H%M%S")
         output_dir = './output'
         os.makedirs(output_dir, exist_ok=True)
+        
+        folder_path = "scraped_urls"
+
+        # Ensure the folder exists; create if it doesn't
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
 
         # launch a browser with headless mode to scrape data
-        browser = await playwright.chromium.launch(headless=True)
+        browser = await playwright.chromium.launch(headless=False)
 
         # create a new page
         page = await browser.new_page()
@@ -34,6 +40,21 @@ async def main():
     
         category_detail_cleaned = [cat.replace('/', ' ') for cat in category_detail_text]
 
+        # 스크랩했던 기사 링크 읽어오기
+        def read_scraped_urls():
+            file_path = os.path.join(folder_path, f"{today_date}_{category_filename}_SCRAPED_URLS_FILE")
+            if not os.path.exists(file_path):
+                return set()
+            with open(file_path, "r") as file:
+                return set(line.strip() for line in file)
+
+
+        # 스크랩한 기사 목록 쓰기
+        def write_scraped_url(url):
+            file_path = os.path.join(folder_path, f"{today_date}_{category_filename}_SCRAPED_URLS_FILE")
+            with open(file_path, "a") as file:
+                file.write(f"{url}\n")
+                
         # 오늘 날짜와 요일을 구하는 함수
         def get_today_date_and_weekday():
             now = datetime.now()
@@ -56,6 +77,8 @@ async def main():
             await page.goto(category_detail_url[i])
 
             today_date, weekday = get_today_date_and_weekday()
+            
+            scraped_urls = read_scraped_urls()
         
             # 오늘 날짜 페이지로 이동
             await page.click(".section_title_btn.is_closed._CALENDAR_LAYER_TOGGLE")
@@ -77,47 +100,50 @@ async def main():
             information = []
             
             for url in urls:
-                await page.goto(url)
-
-                try:
-                    # wait for the 'article' tag to be loaded
-                    await page.wait_for_selector('article')
-
-                    # extract content from the 'article' tag
-                    content = await page.text_content('article')
+                if url not in scraped_urls:
+                    await page.goto(url)
 
                     try:
-                        date = await page.locator('span._ARTICLE_DATE_TIME').all_inner_texts()
-                    except Exception:
-                        date = "N/A"
+                        # wait for the 'article' tag to be loaded
+                        await page.wait_for_selector('article')
 
-                    try:
-                        title = await page.locator('h2.media_end_head_headline').all_inner_texts()
-                    except Exception:
-                        title = "N/A"
+                        # extract content from the 'article' tag
+                        content = await page.text_content('article')
 
-                    try:
-                        media = await page.locator('em.media_end_linked_more_point').all_inner_texts()
-                    except Exception:
-                        media = "N/A"
+                        try:
+                            date = await page.locator('span._ARTICLE_DATE_TIME').all_inner_texts()
+                        except Exception:
+                            date = "N/A"
 
-                    try:
-                        thumbnail_link = await page.get_attribute('img._LAZY_LOADING', 'src')
-                    except Exception:
-                        thumbnail_link = "N/A"
+                        try:
+                            title = await page.locator('h2.media_end_head_headline').all_inner_texts()
+                        except Exception:
+                            title = "N/A"
 
-                    information.append({
-                        'category': category,
-                        'title': title,
-                        'date': date,
-                        'content': content,
-                        'media': media,
-                        'url': url,
-                        'thumbnail_link': thumbnail_link
-                    })
+                        try:
+                            media = await page.locator('em.media_end_linked_more_point').all_inner_texts()
+                        except Exception:
+                            media = "N/A"
 
-                except Exception as e:
-                    print(f"Error occurred while scraping {url}: {str(e)}")
+                        try:
+                            thumbnail_link = await page.get_attribute('img._LAZY_LOADING', 'src')
+                        except Exception:
+                            thumbnail_link = "N/A"
+
+                        information.append({
+                            'category': category,
+                            'title': title,
+                            'date': date,
+                            'content': content,
+                            'media': media,
+                            'url': url,
+                            'thumbnail_link': thumbnail_link
+                        })
+                        
+                        write_scraped_url(url)
+
+                    except Exception as e:
+                        print(f"Error occurred while scraping {url}: {str(e)}")
                 
             print(f"{len(urls)}개 데이터 수집 완료")
                 
